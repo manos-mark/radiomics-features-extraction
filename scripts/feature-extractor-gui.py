02bc71b86befad13d37918d4c0b2ac952a03a5cc
manos <manos-mark@hotmail.com>
2020-06-22 16:49:16 +0300

    feature extractor gui

---
+++ scripts/feature-extractor-gui.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('scripts/radiomics-feature-extractor.ui', self) # Load the .ui file
+        self.show() # Show the GUI
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
