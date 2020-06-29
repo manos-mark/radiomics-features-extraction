6357d05569f09c47ffe9641eab94c50cf41154f4
manos <manos-mark@hotmail.com>
2020-06-29 14:09:08 +0300

    fix bug in pyradiomics_extraction

--- ui/utils/radiomics.py
+++ ui/utils/radiomics.py
@@ -5,19 +5,16 @@ from PyQt5 import QtGui
 
 
 def pyradiomics_extraction(self, image, label, csv_file):
-    if os.path.isfile(image) and os.path.isfile(label):
+    if image and label:
         os.system('pyradiomics ' + image + ' ' + label + ' -o results.csv -f csv --jobs 4')
-    elif os.path.isfile(csv_file):
+    elif csv_file:
         os.system('pyradiomics ' + csv_file + ' -o results.csv -f csv --jobs 4')
     else:
         return
 
     with open('results.csv', "r") as fileInput:
         for row in csv.reader(fileInput):
-            items = [
-                QtGui.QStandardItem(field)
-                for field in row
-            ]
+            items = [QtGui.QStandardItem(field) for field in row]
             self.model.appendRow(items)
 
     os.system('rm results.csv')
