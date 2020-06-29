ac07b3546b0ac74e37d1c452fc3ca950e1062fb9
manos <manos-mark@hotmail.com>
2020-06-29 12:43:47 +0300

    feature extraction

---
+++ ui/utils/radiomics.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import pandas as pd
+from PyQt5 import QtGui
+
+
+def pyradiomics_extraction(self, image, label, csv_file):
+    if os.path.isfile(image) and os.path.isfile(label):
+        os.system('pyradiomics ' + image + ' ' + label + ' -o results.csv -f csv --jobs 4')
+    elif os.path.isfile(csv_file):
+        os.system('pyradiomics ' + csv_file + ' -o results.csv -f csv --jobs 4')
+    else:
+        return
+
+    with open('results.csv', "r") as fileInput:
+        for row in csv.reader(fileInput):
+            items = [
+                QtGui.QStandardItem(field)
+                for field in row
+            ]
+            self.model.appendRow(items)
+
+    os.system('rm results.csv')
