b757d9db885df01dd60fc6d9f1447a1f7ce72cef
manos <manos.mark@gmail.com>
2020-09-09 14:21:36 +0300

    notebooks

--- MaZda/reports_composer.py
+++ MaZda/reports_composer.py
@@ -1,12 +1,10 @@
 import os, glob
 import numpy as np
+import json
+import BeautifulSoup
+import pandas as pd
 
-def compose_files():
-    return
-
-if __name__ == '__main__':
-    path = 'C:\\Users\\manosmark\\Desktop\\radiomic_features\\MaZda\\reports\\'
-
+def compose_files(path):
     os.chdir(path)
     reports = glob.glob('*.xls')
     patient_names = []
@@ -18,7 +16,7 @@ if __name__ == '__main__':
     patient_names = np.unique(patient_names)
 
     for name in patient_names:
-        files.append({'name': name, 'images':[], 'rois': []})
+        files.append({'name': name, 'images':[], 'rois': [], 'report': []})
 
     for report in reports:
         patient_name = (report.split('_')[1])
@@ -32,7 +30,34 @@ if __name__ == '__main__':
             if file.get('name', patient_name):
                 file['images'].append(image_slice_file)
                 file['rois'].append(roi_slice_file)
+                file['report'].append(report)
+                convert_to_xlsx(report)
+                
+    return files
 
-    print(files)
+def convert_to_xlsx(report):
+    with open(report + '.xls') as xml_file:
+        soup = BeautifulSoup(xml_file.read(), 'xml')
+        writer = pd.ExcelWriter('sample.xlsx')
+        for sheet in soup.findAll('Worksheet'):
+            sheet_as_list = []
+            for row in sheet.findAll('Row'):
+                sheet_as_list.append([cell.Data.text if cell.Data else '' for cell in row.findAll('Cell')])
+            pd.DataFrame(sheet_as_list).to_excel(writer, sheet_name=sheet.attrs['ss:Name'], index=False, header=False)
 
-    compose_files()
+        writer.save()
+        
+def export_json(files):
+    with open('files.json', 'w') as fp:
+        json.dump(files, fp)
+
+if __name__ == '__main__':
+    #path = 'C:\\Users\\manosmark\\Desktop\\radiomic_features\\MaZda\\data\\'
+    path = '/home/manos/git/radiomics-features-extraction/MaZda/data'
+    
+    files = compose_files(path)
+
+    export_json(files)
+    
+
+    
