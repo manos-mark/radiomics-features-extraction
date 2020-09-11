742f47771d1e8ea3912318f95cabaaa01ad1267a
manos <manos.mark@gmail.com>
2020-09-11 22:45:16 +0300

    boxplots

--- MaZda/reports_composer.py
+++ MaZda/reports_composer.py
@@ -2,7 +2,7 @@ import os, glob
 import numpy as np
 import json
 
-def compose_files(path):
+def organize_files_to_dict(path):
     os.chdir(path)
     reports = glob.glob('*.xls')
     patient_names = []
@@ -12,25 +12,28 @@ def compose_files(path):
     for report in reports:
         patient_names.append(report.split('_')[1])
     patient_names = np.unique(patient_names)
-
+    
     for name in patient_names:
-        files.append({'name': name, 'images':[], 'rois': [], 'report': []})
+        files.append({'name': name, 'images':[], 'rois': [], 'reports': []})
 
     for report in reports:
-        patient_name = (report.split('_')[1])
+        patient_name = report.split('_')[1]
+        
         image_slice = patient_name + "_slice_" + report.split('_')[-1].split('.')[0]
         image_slice_file = image_slice + '.dcm'
 
         roi_slice = patient_name + "_roi_slice_" + report.split('_')[-1].split('.')[0]
         roi_slice_file = roi_slice + '.bmp'
 
-        for file in files:
-            if file.get('name', patient_name):
-                file['images'].append(image_slice_file)
-                file['rois'].append(roi_slice_file)
-                file['report'].append(report)
-                
+        file = next((file for file in files if file['name'] == patient_name), None)
+
+        if file is not None:
+            file['images'].append(image_slice_file)    
+            file['rois'].append(roi_slice_file)
+            file['reports'].append(report)
+        
     return files
+
 
 def export_json(files):
     with open('files.json', 'w') as fp:
@@ -40,9 +43,10 @@ if __name__ == '__main__':
     #path = 'C:\\Users\\manosmark\\Desktop\\radiomic_features\\MaZda\\data\\'
     path = '/home/manos/git/radiomics-features-extraction/MaZda/data'
     
-    files = compose_files(path)
+    files = organize_files_to_dict(path)
 
     export_json(files)
+    
     
 
     
